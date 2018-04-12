6252832435aee148f7cb2a8aeedd6e7c293b948d
Sam Tipton <stipton@rmn.com>
2018-04-12 09:53:29 -0500

    add SearchRepository

--- app/src/main/java/com/whaleshark/basicarch/viewmodel/SearchViewModel.java
+++ app/src/main/java/com/whaleshark/basicarch/viewmodel/SearchViewModel.java
@@ -1,22 +1,15 @@
 package com.whaleshark.basicarch.viewmodel;
 
 import android.arch.lifecycle.LiveData;
-import android.arch.lifecycle.MediatorLiveData;
 import android.arch.lifecycle.MutableLiveData;
+import android.arch.lifecycle.Transformations;
 import android.arch.lifecycle.ViewModel;
 
-import com.whaleshark.basicarch.api.FoodPuppyService;
+import com.whaleshark.basicarch.data.SearchRepository;
 import com.whaleshark.basicarch.model.Recipe;
-import com.whaleshark.basicarch.model.RecipeSearchResult;
-import com.whaleshark.basicarch.util.LiveDataCallAdapterFactory;
+import com.whaleshark.basicarch.util.Objects;
 
 import java.util.List;
-
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
-import retrofit2.Retrofit;
-import retrofit2.converter.gson.GsonConverterFactory;
 
 /**
  * @author stipton
@@ -24,34 +17,20 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class SearchViewModel extends ViewModel {
 
-    private final FoodPuppyService foodPuppyService = FoodPuppyService.Factory.create();
-
+    private SearchRepository searchRepository = new SearchRepository();
     private MutableLiveData<String> query = new MutableLiveData<>();
-    private MediatorLiveData<List<Recipe>> resultsMediator = new MediatorLiveData<>();
+    private LiveData<List<Recipe>> results;
 
     public SearchViewModel() {
-
-        resultsMediator.addSource(query, query -> {
-            foodPuppyService.searchRecipes(query).enqueue(new Callback<RecipeSearchResult>() {
-                @Override
-                public void onResponse(Call<RecipeSearchResult> call, Response<RecipeSearchResult> response) {
-                    resultsMediator.postValue(response.body().results);
-                }
-
-                @Override
-                public void onFailure(Call<RecipeSearchResult> call, Throwable t) {
-                    resultsMediator.postValue(null);
-                }
-            });
-        });
+        results = Transformations.switchMap(query, searchQuery -> searchRepository.searchRecipes(searchQuery));
     }
 
     public LiveData<List<Recipe>> getResults() {
-        return resultsMediator;
+        return results;
     }
 
     public void setQuery(String query) {
-        if (this.query.getValue() == null || !this.query.getValue().equals(query)) {
+        if (!Objects.equals(this.query.getValue(), query)) {
             this.query.setValue(query);
         }
     }
